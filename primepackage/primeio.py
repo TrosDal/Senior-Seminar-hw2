d42fe7629a739c08975660269ac6d33d72f07f3e
René Zambrana <rxzambrana@gmail.com>
2020-03-06 01:19:14 -0500

    First Commit.

---
+++ primepackage/primeio.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""Write here."""
+
+def write_primes(num_list, file_name):
+    """Takes a list of prime numbers and writes them to a csv file."""
+    print(7)
+
+def read_primes(file_name):
+    """Reads prime numbers from a csv file and places them into a list, which it returns."""
+    print(9)
---
+++ primepackage/primemodule.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+"""Contains the functions that will check if a number is prime and
+another that will find the first n primes and put them in a list."""
+
+def isPrime(n):
+    """Checks if a number is prime, returning a boolean."""
+    if (n == 2):
+        return True
+
+    for i in range(2, n):
+        if (n % i == 0):
+            return False
+    return True
+
+def getNPrime(num):
+    """Returns the first n primes."""
+    count = 0
+    number = 2
+    primes = list(range(num))
+    while (count < num):
+        if (isPrime(number)):
+            primes[count] = number
+            count = count + 1
+            if (number == 2):
+                number = number + 1
+            else:
+                number = number + 2
+    print(list)
+
+getNPrime(5)
